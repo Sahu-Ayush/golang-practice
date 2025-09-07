b6a1153009633ee195498fdc1d5162647fd58fc5
Ayush Sahu <ayushsahu.pro@gmail.com>
2025-09-07 16:26:54 +0530

    Wrong type entered

---
+++ phase1_basics/04_input_from_user/01_fmt_Scanln/practice_04.go
@@ -0,0 +1,27 @@
+// Q4. Wrong type entered
+
+// Ask the user for their age (int).
+
+// Input: twenty
+
+// What happens to the variable? What error is returned?
+
+package main
+
+import "fmt"
+
+func main() {
+	var age int
+
+	fmt.Print("Enter you age: ")
+	n, err := fmt.Scanln(&age)
+
+	fmt.Printf("Scanned: %d\n", n)
+	if err != nil {
+		fmt.Print(err, "\n")
+	} else {
+		fmt.Printf("Your age is %d\n", age)
+	}
+}
+
+// Ans: expected integer
