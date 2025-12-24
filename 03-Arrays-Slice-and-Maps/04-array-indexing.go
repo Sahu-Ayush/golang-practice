402e735dd51b60c7dce907111ad7c9e6913a1e39
Ayush Sahu <ayushsahu.pro@gmail.com>
2025-12-25 02:11:26 +0530

    Array indexing - accessing index using by its index

---
+++ 03-Arrays-Slice-and-Maps/04-array-indexing.go
@@ -0,0 +1,16 @@
+// Access an element of an array by its index
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var grades [5]int = [5]int{10, 20, 30, 40, 50}
+	fmt.Println("grades[2]: ", grades[2])
+
+}
+
+/* Output:
+grades:  [10 20 30 40 50]
+*/
