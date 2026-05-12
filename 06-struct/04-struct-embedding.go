77c7675b646394880f06353599956ee70cd96ce0
Sahu-Ayush <ayush@ayushsahu.dev>
2026-05-12 22:43:37 +0530

    04-struct-embedding.go

---
+++ 06-struct/04-struct-embedding.go
@@ -0,0 +1,26 @@
+package main
+
+type Address struct {
+	City    string
+	Country string
+}
+
+type Employee struct {
+	Name    string
+	Address // This is struct embedding. Employee now has the fields of Address.
+}
+
+func main() {
+	emp := Employee{
+		Name: "Ayush S",
+		Address: Address{
+			City:    "Bangalore",
+			Country: "India",
+		},
+	}
+
+	// We can access the fields of the embedded struct directly from the Employee struct.
+	println("Name:", emp.Name)
+	println("City:", emp.City)       // Accessing City directly from Employee
+	println("Country:", emp.Country) // Accessing Country directly from Employee
+}
